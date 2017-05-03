ccdf4a9b92ad7718bdf2b80c23c8f648d20c29fe
Wiktor Korol <wiktorkorol@icloud.com>
2017-05-03 20:07:40 +0200

    Initial Commit

---
+++ SaveTheKittensExceptTheairlessOnes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittensExceptTheairlessOnes
+//
+//  Created by Wiktor Korol on 03.05.2017.
+//  Copyright © 2017 wkorol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
